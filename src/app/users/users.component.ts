48f68099a412b638f9fdf1af2f46200f53ebc986
ValeriiBihun <bvp3745@gmail.com>
2021-06-06 14:48:26 +0300

    angular-project ls1

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {UserService} from "../services/user.service";
+import {User} from "../models/user";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  //private httpClient:HttpClient;
+
+ // constructor(httpClient:HttpClient) {
+ //   this.httpClient=httpClient;
+ // }
+
+  users:User[];
+
+  constructor(private userService:UserService){
+
+  }
+
+  ngOnInit(): void {
+this.userService.getUsers().
+subscribe(value => this.users=value)
+  }
+
+}
